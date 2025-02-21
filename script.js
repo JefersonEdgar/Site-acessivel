437e38c1be5b50335b7838644702ae72beed136b
JefersonEdgar <134607846+JefersonEdgar@users.noreply.github.com>
2025-02-21 15:51:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -24,4 +24,8 @@ document.addEventListener('DOMContentLoaded', function () {
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
     })
+    const alternaContraste = document.getElementById('alterna-contraste')
+    alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
+})
 })
